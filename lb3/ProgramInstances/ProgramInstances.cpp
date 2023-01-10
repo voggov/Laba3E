6e385a02f457518280d667b729f266f480249505
Илья Вожжов <voggov1@mail.ru>
2023-01-10 10:24:47 +0300

    3laba

---
+++ lb3/ProgramInstances/ProgramInstances.cpp
@@ -0,0 +1,31 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <iostream>
+#include <locale.h>
+void main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    HANDLE File = OpenFileMappingA(FILE_MAP_ALL_ACCESS, TRUE, "Local\\MapCounter");
+    PCHAR MapViewOfCounter = (PCHAR)MapViewOfFile(File, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+
+    HANDLE Mutex = OpenMutexA(NULL, FALSE, "Local\\Mutex");
+
+    WaitForSingleObject(Mutex, INFINITE);
+    INT inc = atoi(MapViewOfCounter);
+    inc++;
+    snprintf(MapViewOfCounter, sizeof(MapViewOfCounter), "%d", inc);
+    std::cout << "Номер экзмепляра - " << MapViewOfCounter << std::endl;
+
+    ReleaseMutex(Mutex);
+
+    system("pause");
+
+    WaitForSingleObject(Mutex, INFINITE);
+    inc = atoi(MapViewOfCounter);
+    inc--;
+    snprintf(MapViewOfCounter, sizeof(MapViewOfCounter), "%d", inc);
+  
+    ReleaseMutex(Mutex);
+    CloseHandle(File);
+}
---
+++ lb3/lb3/lb3.cpp
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <Windows.h>
+#include <iostream>
+#include <locale>
+#define NumberOfMonitors 3
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    HANDLE File = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, 256, "Local\\MapCounter");
+    PCHAR MapViewOfCounter = (PCHAR)MapViewOfFile(File, FILE_MAP_ALL_ACCESS, 0, 0, 0);
+    snprintf(MapViewOfCounter, sizeof(MapViewOfCounter), "0");
+
+    HANDLE Mutex = CreateMutexA(0, FALSE, "Local\\Mutex"); 
+    
+    STARTUPINFOA si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    ZeroMemory(&pi, sizeof(pi));
+
+    for (int i = 0; i < NumberOfMonitors; i++)
+    CreateProcessA(NULL,(LPSTR)"ProgramInstances",NULL,NULL,TRUE,CREATE_NEW_CONSOLE,NULL,NULL,&si,&pi);
+
+    while (true)
+    {
+        Sleep(80);
+        system("cls");
+        std::cout << "Текущее колличество запущенных экзмепляров - " << MapViewOfCounter;
+        if (strcmp(MapViewOfCounter, "0") == 0)
+            break;
+    }
+
+    UnmapViewOfFile(MapViewOfCounter);
+    CloseHandle(File);
+    CloseHandle(Mutex);
+
+    printf("\n");
+    system("pause");
+}
+
